264da8818b9d4ed7a3ee73d28890c6d4301045c8
Fredrik Ekre <ekrefredrik@gmail.com>
2021-07-12 21:04:31 +0200

    print bt

--- src/Expanders.jl
+++ src/Expanders.jl
@@ -588,7 +588,7 @@ function Selectors.runner(::Type{ExampleBlocks}, x, page, doc)
                     ```$(x.language)
                     $(x.code)
                     ```
-                    """, c.value)
+                    """, exception=(c.value, c.backtrace))
                 page.mapping[x] = x
                 return
             end
